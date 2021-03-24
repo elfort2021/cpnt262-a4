5b4e899272eeea2e1d4f9f8ad3cf20c0324f83a1
elfort2021 <ellie.forte9@gmail.com>
2021-03-24 14:05:30 -0600

    images, gallery transferred

---
+++ public/js/script.js
@@ -0,0 +1,130 @@
+'use strict'
+
+const image = [{
+    id: 1,
+    title: 'Doorways',
+    description: 'Doorways are portals of entry to the unknown, often leading to somewhere unexpected.',
+    width: 683,
+    pathURL: 'images/img1.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/Door',
+    credit: 'Filip Kominik via Unsplash',
+    creditURL: 'https://unsplash.com/photos/IHtVbLRjTZU'
+  },
+  {
+    id: 2,
+    title: 'Watermelon Peperomia',
+    description: 'Tropical Plant Leafs of Peperomia with silver highlight, a specific growth pattern to subsect of plant family',
+    width: 683,
+    pathURL: 'images/img2.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/Peperomia',
+    credit: 'Ren Ran via Unsplash',
+    creditURL: 'https://unsplash.com/photos/bBiuSdck8tU'
+  },
+  {
+    id: 3,
+    title: 'Fiddle Leaf Fig',
+    description: 'A commonly tricky houseplant despite its popularity, the fiddle-leaf fig does not respond well to being moved, especially from a spot where it is thriving.',
+    width: 683,
+    pathURL: 'images/img3.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/Ficus_lyrata',
+    credit: 'Kendal via Unsplash',
+    creditURL: 'https://unsplash.com/photos/TW2bfT_tWDI'
+
+  },
+  {
+    id: 4,
+    title: 'Monstera',
+    description: 'Large dark green monstera leaf with healthy fenstration lines indicating age',
+    width: 683,
+    pathURL: 'images/img4.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/Monstera',
+    credit: 'Paula Russel via Unsplash',
+    creditURL: 'https://unsplash.com/photos/8FSJjOb1nUc'
+  },
+  {
+    id: 5,
+    title: 'Helianthus',
+    description: 'Sunflowers Growing Together in a heliotropic fashion',
+    width: 1024,
+    pathURL: 'images/img5.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/Heliaonthus',
+    credit: 'Gerome Brunaeu via Unsplash',
+    creditURL: 'https://unsplash.com/photos/RPmWEtZLh7U'
+  },
+  {
+    id: 6,
+    title: 'Greenhouses',
+    description: 'The interior of a greenhouse exposed to sunlight becomes significantly warmer than the external temperature, protecting its contents in cold weather.',
+    width: 683,
+    pathURL: 'images/img6.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/Greenhouse',
+    credit: 'Elsa Noblet via Unsplash',
+    creditURL: 'https://unsplash.com/photos/r37oKn9cW-s'
+  },
+  {
+    id: 7,
+    title: 'Succulents',
+    description: 'Small cacti-like plants in pots that you can forget to water',
+    width: 1024,
+    pathURL: 'images/img7.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/Succulent_plant',
+    credit: 'Annie Sprat via Unsplash',
+    creditURL: 'https://unsplash.com/photos/ncQ2sguVlgo'
+  },
+  {
+    id: 8,
+    title: 'Fern',
+    description: 'A fern leaf being held up against a forest backdrop',
+    width: 683,
+    pathURL: 'images/img8.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/Fern',
+    credit: 'Olena Sergienko via Unsplash',
+    creditURL: 'https://unsplash.com/photos/r0M9HrfJMBM'
+  },
+  {
+    id: 9,
+    title: 'Rubber Tree',
+    description: 'Rubber Tree Leafs and Stem, an intermediate level of growth from a moderately aged plant',
+    width: 683,
+    pathURL: 'images/img9.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/Ficus_elastica',
+    credit: 'Scott Webb via Unsplash',
+    creditURL: 'https://unsplash.com/photos/BLBCj6dxaSE'
+  }
+];
+
+// This took me like 3 different ways and 19 google searches to figure out. 
+
+// link to html 
+const imageContainer = document.querySelector(".image-gallery");
+
+// the loop
+
+for (let i = 0; i < image.length; i++) {
+
+  //container 
+  const figure = document.createElement('figure');
+  imageContainer.appendChild(figure);
+
+
+  //image 
+
+  const figImage = document.createElement('img')
+  figImage.src = `${image[i].pathURL}`;
+  figImage.alt = `${image[i].description}`;
+  figImage.style.maxWidth = `${image[i].width}`;
+  // realized I hadn't linked these. 
+  figImage.credit =  `${image[i].credit}`;
+  figImage.creditURL = `${image[i].creditURL}`;
+  figure.appendChild(figImage);
+
+  //title 
+  const caption = document.createElement('h2');
+  caption.innerText = `${image[i].title}`;
+  figure.appendChild(caption);
+
+
+  const figCaption = document.createElement('figcaption');
+  figCaption.innerText = `${image[i].description}`;
+  figure.appendChild(figCaption)
+};
